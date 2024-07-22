41c7af7f10917ad1233e439c5a5bc31ecb8168e8
Contactsushil <contact.sushilpandey@gmail.com>
2024-07-22 20:27:07 +0530

    Ollama integration first version

--- Server/main.py
+++ Server/main.py
@@ -1,7 +1,71 @@
 from fastapi import FastAPI
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from dotenv import load_dotenv
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import Chroma
+from operator import itemgetter
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.llms import Ollama
 
 app=FastAPI()
 
+load_dotenv()
+loader = PyPDFDirectoryLoader('data/')
+print("1")
+documents = loader.load()
+print("2")
+print(documents)
+print("3")
+print(documents[0].page_content)
+
+
+text_splitter = CharacterTextSplitter(
+    separator="\n\n",
+    chunk_size=500,
+    chunk_overlap=50,
+    length_function=len,
+    is_separator_regex=False,
+)
+print("4")
+
+chunks = text_splitter.split_documents(documents)
+chunks
+chunks[7]
+print("5")
+
+index = Chroma.from_documents(chunks, OllamaEmbeddings(model="mxbai-embed-large",show_progress=True))
+print("6")
+
+retriever = index.as_retriever()
+
+template = """
+
+Answer the following question:
+Question: {question}
+
+Answer the question based only on the following context:
+{context}
+
+"""
+print("7")
+prompt = ChatPromptTemplate.from_template(template)
+model = Ollama(model="qwen2:0.5b")
+print("8")
+
+rag_chain = (
+    {
+        "context": itemgetter("question") | retriever,
+        "question": itemgetter("question")
+    }
+    | prompt
+    | model
+    | StrOutputParser()
+)
+
+
+
 @app.get('/')
 async def root():
-    return {"message": "Hello World"}
+    return {"message": rag_chain.invoke({"question": "What is evil quartet? explain properly"})}
