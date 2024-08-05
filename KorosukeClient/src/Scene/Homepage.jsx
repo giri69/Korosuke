601f45107b5e5f5e0706b808496d91e9a3d6e236
giridhar <giridharpp07@gmail.com>
2024-08-05 08:40:58 +0530

    file structure

--- KorosukeClient/src/Scene/Homepage.jsx
+++ KorosukeClient/src/Scene/Homepage.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const Homepage = () => {
   return (
     <>
-    Homepage
+    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+        <Footer />
+      </div>
+
+      <ButtonGradient />
     </>
   )
 }
